d24488bb9012970dcfc17b55dcc00ac85bca9e0a
youngyifei <603350333@qq.com>
2016-12-01 19:18:03 +0800

    今天的成果

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,92 @@
 <template>
   <div id="app">
-  <mu-appbar title="Title">
-    <mu-icon-button icon='menu' slot="left"/>
-    <mu-icon-button icon='expand_more' slot="right"/>
-  </mu-appbar>
-    <img src="./assets/logo.png">
-    <hello></hello>
-    <mu-paper style="max-width: 376px; ">
-    <mu-bottom-nav :value="bottomNav" shift @change="handleChange">
-      <mu-bottom-nav-item value="movies" title="Movies" icon="ondemand_video"/>
-      <mu-bottom-nav-item value="music" title="Music" icon="music_note"/>
-      <mu-bottom-nav-item value="books" title="Books" icon="books"/>
-      <mu-bottom-nav-item value="pictures" title="Pictures" icon="photo"/>
-    </mu-bottom-nav>
-  </mu-paper>
+    <div id="app-bar">
+      <mt-header title="大客户系统" style="height:50px;">
+        <router-link to="/" slot="left">
+          <mt-button icon="more" @click="popupTaggle" >返回</mt-button>
+          <mt-button @click="handleClose">关闭</mt-button>
+        </router-link>
+        <mt-button icon="more" slot="right"></mt-button>
+      </mt-header>
+      <mt-popup v-model="popupVisible" position="left" style="height:100%;width:200px;">
+        <div style="height:100%;">
+          <mt-cell title="我的报单" is-link></mt-cell>
+          <mt-cell title="我的客户" is-link></mt-cell>
+          <mt-cell title="我的周报" is-link></mt-cell>
+          <mt-cell title="我的信息" is-link></mt-cell>
+        </div>
+      </mt-popup>
+    </div>
+    <div style="margin:64px 0px;width:100%;">
+      <img src="./assets/logo.png">
+      <hello></hello>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import Hello from './components/Hello'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  },
-  data () {
-    return {
-      bottomNav: 'movies',
-      bottomNavColor: 'movies'
-    }
-  },
-  methods: {
-    handleChange (val) {
-      this.bottomNav = val
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    },
+    data () {
+      return {
+        bottomNav: 'movies',
+        bottomNavColor: 'movies',
+        popupVisible: false,
+        data: {
+          token: '74E56A42-4786-4F86-87FC-7685294F34BC',
+          workCode: 'WA01658',
+          key: ''}
+      }
+    },
+    methods: {
+      handleChange (val) {
+        this.bottomNav = val
+      },
+      handleClose () {
+      },
+      popupTaggle () {
+        this.popupVisible = !this.popupVisible
+        this.$http.get('/api/WUApi/GetRoleList')
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.demo-flat-button {
-  margin: 12px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /*margin-top: 60px;*/
+  }
+  #app-bar{
+    position: fixed;
+    width: 100%;
+    top:0px;
+    left:0px;
+    transition: all .45s cubic-bezier(.23,1,.32,1);
+  }
+  #app-nav{
+    position: fixed;
+    width: 100%;
+    bottom:0px;
+    left:0px;
+    transition: all .45s cubic-bezier(.23,1,.32,1);
+  }
+  .demo-flat-button {
+    margin: 12px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
-import MuseUI from 'muse-ui'
-import 'muse-ui/dist/muse-ui.css'
-import './assets/fonts.css'
-import './assets/icon.css'
-Vue.use(MuseUI)
-
+import MintUI from 'mint-ui'
+import axios from 'axios'
+import 'mint-ui/lib/style.css'
+import './assets/mint-icon.css'
+Vue.use(VueRouter)
+Vue.use(MintUI)
+axios.defaults.baseURL = 'http://127.0.0.1:8081'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$http = axios
+Vue.mixin({
+  data () {
+    return {
+      axios: axios
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
+  methods: {
+
+  }
 })
